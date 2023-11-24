0520da9334ce80d1729fa345f84c3a68853d3717
john <john@JOHN>
2023-11-24 07:24:17 -0800

    Add project files.

---
+++ EcommerceWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Required]
+        [MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100, ErrorMessage ="Enter number between 0-100") ]
+        public int DisplayOrder { get; set; }
+
+    }
+}
